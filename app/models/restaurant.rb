be7649a7f847d7caa1ed75ee0f460dad03f8056d
Trey Booth <tbooth9@gmail.com>
2018-04-04 13:46:30 +0800

    Rails new with frontend and form gems

---
+++ app/models/restaurant.rb
@@ -0,0 +1,13 @@
+class Restaurant < ApplicationRecord
+    def change
+      create_table :restaurants do |t|
+        t.string :name
+        t.string :address
+        t.text :description
+        t.integer :stars
+
+        t.timestamps
+      end
+    end
+  end
+end
